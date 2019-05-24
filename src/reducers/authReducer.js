f3906266ba285552bb113a779fa8ff313429a1dc
felixkiryowa <franciskiryowa68@gmail.com>
2019-05-24 20:25:31 +0300

    ch(Redux) Set up redux in the Application
    - Set up redux workflow in the Application
    
    [Finishes #166239178]

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    isAuthenticated: false,
+    user: {
+
+    },
+};
+
+export default function (state = initialState, action) {
+    switch (action) {
+        default:
+            return state;
+    }
+}
