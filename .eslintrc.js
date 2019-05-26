4ea6061314cb21d93728ff2dfd72c3747cf3ddd0
felixkiryowa <franciskiryowa68@gmail.com>
2019-05-26 11:27:42 +0300

    ch(Deploy) Deploying Application on Heroku
    - Deploying the application on Heroku such that it can be accessed from anywhere
    
    [Finishes #166239242]

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "jest","react"
+    ],
+    "rules": {
+        'no-console': 'off',
+        'react/prop-types': 'off',
+        "react/jsx-uses-react": 1,
+        "react/react-in-jsx-scope": 1,
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        "max-len": [1, 80, 2]
+    },
+    "parser": "babel-eslint",
+};
